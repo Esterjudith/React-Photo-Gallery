005f83f4a12128457b1c1431cd12e92d3b233b38
Esterjudith <106325339+Esterjudith@users.noreply.github.com>
2023-01-06 15:12:56 -0700

    connected the server to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useState } from 'react'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+   const[image, setImage] = useState(null)   
+
+   const handleUpload = () => {
+    console.log("handle uploads")
+    axios.post('http://localhost:3001/image-upload', image)
+   }
+   
+   const getFileInfo = (e) => {
+     console.log('File info working!')
+     console.log(e.target.files[0])
+     const formData = new FormData()
+     formData.append('my-image-file', e.target.files[0], e.target.files[0].name)
+     setImage(formData)
+   }
+    return (
+        <div className="App">
+       
+            <h1>Image upload</h1>
+            <input type="file" onChange={getFileInfo} />
+            <button onClick={handleUpload}>Upload Image</button>
+          
+        </div>
+    );
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const logger = require('morgan');
 const connectDB = require('../server/config/database');
+const cors = require('cors')
+const multer = require('multer')
+const path = require('path')
+
+
+const corsOrigin = 'http://localhost:3000'
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
@@ -9,15 +15,42 @@ require("dotenv").config({ path: "./config/.env" });
 //Connect to Database
 connectDB();
 
+app.use(express.static(__dirname + '../..'))
+
+app.use(cors({
+    origin: [corsOrigin],
+    credentials: true //enable set cookie
+}))
+
 //Body Parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const imageUploadPath = 'C:\\Users\\Ben\\OneDrive\\Documents\\Interview Project\\React-Photo-Gallery\\server\\uploaded_Images'
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, imageUploadPath)
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${file.fieldname}_dateVal_${Date.now()}_${file.originalname}`)
+    }
+})
+const imageUpload = multer({ storage: storage })
+
 
 //Logging
 app.use(logger("dev"));
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello from server!' })
+})
+
+app.post('/image-upload', imageUpload.array("my-image-file"), (req, res) => {
+    console.log('req.body', req.body)
+    console.log('Got a POST request')
+    res.send('Got a POST request')
 })
 
 //Server Running
