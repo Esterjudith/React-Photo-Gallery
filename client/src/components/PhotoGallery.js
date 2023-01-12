0b32c9beec489b456614fdfb343d147d8ed2c574
Esterjudith <106325339+Esterjudith@users.noreply.github.com>
2023-01-11 17:55:26 -0700

    updated

--- client/src/components/PhotoGallery.js
+++ client/src/components/PhotoGallery.js
@@ -6,37 +6,37 @@ import "yet-another-react-lightbox/styles.css";
 
 
 function PhotoGallery({ result }) {
- const [open, setOpen] = useState(false)
+    const [open, setOpen] = useState(false)
 
-  
-  const items = result.map(({image}) => ({
-    src: image,
-    width: 320,
-    height: 174
-  }))    
-  
 
-  const slides = result.map(({ image }) =>({
-    src:image    
-  }))
+    const items = result.map(({ image }) => ({
+        src: image,
+        width: 320,
+        height: 174
+    }))
 
-//   const handleClick = (index, slides) => setIndex(index);
 
-  return (
-    <div>
-      <Gallery
-        images={items}
-        onClick={()=> setOpen(true)}
-        enableImageSelection={false}
-      />
-      <Lightbox
-        slides={slides}
-        open={open}
-        // index={index}
-        close={() => setOpen(false)}
-      />
-    </div>
-  );
+    const slides = result.map(({ image }) => ({
+        src: image
+    }))
+
+    //   const handleClick = (index, slides) => setIndex(index);
+
+    return (
+        <div>
+            <Gallery
+                images={items}
+                onClick={() => setOpen(true)}
+                enableImageSelection={false}
+            />
+            <Lightbox
+                slides={slides}
+                open={open}
+                // index={index}
+                close={() => setOpen(false)}
+            />
+        </div>
+    );
 }
 
 export default PhotoGallery
