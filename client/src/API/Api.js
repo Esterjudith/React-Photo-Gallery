5dfc82d2e71f0ae3da42b668da62c7d83a1a5f42
Esterjudith <106325339+Esterjudith@users.noreply.github.com>
2023-01-16 15:50:08 -0700

    update

--- client/src/API/Api.js
+++ client/src/API/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = process.env.REACT_APP_SERVER_URL
+// const URL = process.env.REACT_APP_SERVER_URL
 
 export const getData = async ({ setGetDataLoading, setResult }) => {
     console.log("getData")
